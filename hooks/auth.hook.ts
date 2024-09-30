904a7167be46b4affa5207d047739fee98919be0
noyon-podder <noyon.podder7@gmail.com>
2024-09-30 23:57:35 +0600

    tanstack query add

--- hooks/auth.hook.ts
+++ hooks/auth.hook.ts
@@ -1,1 +1,25 @@
-export const userRegistration = () => {};
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { registerUser } from "@/services/AuthService";
+import { useMutation } from "@tanstack/react-query";
+import { FieldValues } from "react-hook-form";
+import { useToast } from "./use-toast";
+
+export const useRegistration = () => {
+  const { toast } = useToast();
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_REGISTRATION"],
+    mutationFn: async (userData) => await registerUser(userData),
+    onSuccess: () => {
+      toast({
+        variant: "destructive",
+        description: "User Create SuccessFully.",
+      });
+    },
+    onError: (error) => {
+      toast({
+        variant: "destructive",
+        description: error.message,
+      });
+    },
+  });
+};
--- lib/Provider/index.tsx
+++ lib/Provider/index.tsx
@@ -4,12 +4,14 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
 
 const queryClient = new QueryClient();
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <NextThemesProvider {...props}>{children}</NextThemesProvider>;
     </QueryClientProvider>
   );
